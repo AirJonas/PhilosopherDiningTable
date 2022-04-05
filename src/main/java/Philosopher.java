49d275e8a2ff120574ec99dff5310babcd4d64dc
aironas <Aironasverbyla@gmail.com>
2022-04-05 23:33:16 +0200

    added Philosopher class

--- src/main/java/Philosopher.java
+++ src/main/java/Philosopher.java
@@ -1,16 +1,66 @@
 /**
- * this class represents a thread whish holds a philosopher
- *
+ * this class represents a thread which holds a philosopher
  *
  * @author Aironas
  */
 public class Philosopher implements Runnable {
   private int myId;
   private int timesToEat;
-  private moniter mon;
+  private Monitor mon;
+  private Thread t;
+  private int sleepLenght;
+
+  Philosopher(int id, int numToEat, Monitor m){
+    this.myId = id;
+    this.timesToEat = numToEat;
+    this.mon = m;
+    sleepLenght = 10;
+    t = new Thread(this);
+    t.start();
+  }
 
   @Override
   public void run() {
+    int count = 1;
+    while(count <= timesToEat ){
+      mon.PickUp(myId);
+      eat(count);
+      mon.PutDown(myId);
+      ++count;
+    }
+  }
 
+  void eat(int count){
+    System.out.format("Philosopher %d eats (%d times)\n", myId+1, count);
+    try {
+      Thread.sleep(sleepLenght);
+    }
+    catch (InterruptedException e) {}
+  }
+
+  public static void main(String[] args) {
+    int numOfPhilosophers = 5;
+    int timesToEat = 5;
+
+    Monitor mon = new Monitor(numOfPhilosophers);
+    Philosopher [] p = new Philosopher[numOfPhilosophers];
+
+    System.out.println("Dinner is starting!");
+    System.out.println("");
+
+    // Start philosopher threads.
+    for(int i = 0; i < numOfPhilosophers; i++)
+      p[i] = new Philosopher(i, timesToEat, mon);
+
+    // Suspend the current thread until the philosophers have not done.
+    for(int i = 0; i < numOfPhilosophers; i++)
+      try {
+        p[i].t.join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    System.out.println("");
+    System.out.println("Dinner is over!");
   }
 }
+
